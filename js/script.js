2e4be230d35aa325de31c82ce43f5f0d840005d8
Carol <carolkaji@gmail.com>
2020-05-15 16:34:57 -0300

    Criado função para gerar aleatoriamente código hexadecimal de cores

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 // Atribuindo o JSON para a array ficha:
-const ficha = JSON.parse(dados);
+const FICHA = JSON.parse(dados);
 // console.log(ficha);
 
+// Criando cores aleatórias:
+const ARRAY_HEXADECIMAL=[1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+function randomColor() {
+  let color = '';
+  for(let i = 0; i < 6; i++){
+    let numero = Math.round((Math.random()*16));
+    color += ARRAY_HEXADECIMAL[numero];
+  }
+  return `#${color}`;
+}
+
 window.addEventListener('load', () =>{
-  for(let i = 0; i < ficha.length; i++) {
+  for(let i = 0; i < FICHA.length; i++) {
     container.innerHTML += 
     `<div class="card">
-      <img src="${ficha[i].download_url}">
+      <img src="${FICHA[i].download_url}">
       <div class="card-description">
         <ul>
-          <li><b>ID:</b> ${ficha[i].id}</li>
-          <li><b>Nome:</b> ${ficha[i].author}</li>
-          <li><b>URL:</b> ${ficha[i].url}</li>
+          <li><b>ID:</b> ${FICHA[i].id}</li>
+          <li><b>Nome:</b> ${FICHA[i].author}</li>
+          <li><b>URL:</b> ${FICHA[i].url}</li>
         <ul>
       </div>
     </div>`
   }
+  
 });
