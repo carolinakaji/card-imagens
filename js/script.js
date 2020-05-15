689ffae0b6ab6841c390bf710a8f0a1fb47267c0
Carol <carolkaji@gmail.com>
2020-05-15 17:17:13 -0300

    Ajuste na função randomColor, a variável numero deve variar de 0 a 15, e ajuste na constante ARRAY_HEXADECIMAL, inserido o 0 como index[0]. Adição do parágrafo para inserir o código da cor. Ajuste no loop para inserir a cor na div card e inserir o código da cor

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ const FICHA = JSON.parse(dados);
 // console.log(ficha);
 
 // Criando cores aleatórias:
-const ARRAY_HEXADECIMAL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+const ARRAY_HEXADECIMAL = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 function randomColor() {
   let color = '';
-  for (let i = 0; i < 6; i++) {
-    let numero = Math.round((Math.random() * 16));
+  for (let i = 1; i < 7; i++) {
+    let numero = Math.round((Math.random() * 15));
+    // console.log(numero);
     color += ARRAY_HEXADECIMAL[numero];
   }
   return `#${color}`;
@@ -24,13 +25,18 @@ window.addEventListener('load', () => {
     <li><b>Nome:</b> ${FICHA[i].author}</li>
     <li><b>URL:</b> ${FICHA[i].url}</li>
     <ul>
+    <p></p>
     </div>
     </div>`
   }
 
   // Lembrar que getElementsByClassName, gera uma array. Para acessar cada card e alterar o css, deve-se rodar um loop acessando cada card:
   let card = document.getElementsByClassName('card');
+  let corCard = document.getElementsByTagName('p');
   for (let i = 0; i < card.length; i++) {
-    card[i].style.backgroundColor = randomColor();
+    let color = randomColor();
+    card[i].style.backgroundColor = color;
+    corCard[i].innerHTML = color;
+    // console.log(color);
   }
 });
