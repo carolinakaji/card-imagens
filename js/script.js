82e7383895ac9a0f095ffb9e5668b6504ef94e59
Carol <carolkaji@gmail.com>
2020-05-15 16:40:29 -0300

    Criado expressão para atribuir cores aleatórias para os cards

--- js/script.js
+++ js/script.js
@@ -3,29 +3,33 @@ const FICHA = JSON.parse(dados);
 // console.log(ficha);
 
 // Criando cores aleatórias:
-const ARRAY_HEXADECIMAL=[1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+const ARRAY_HEXADECIMAL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 function randomColor() {
   let color = '';
-  for(let i = 0; i < 6; i++){
-    let numero = Math.round((Math.random()*16));
+  for (let i = 0; i < 6; i++) {
+    let numero = Math.round((Math.random() * 16));
     color += ARRAY_HEXADECIMAL[numero];
   }
   return `#${color}`;
 }
 
-window.addEventListener('load', () =>{
-  for(let i = 0; i < FICHA.length; i++) {
-    container.innerHTML += 
-    `<div class="card">
-      <img src="${FICHA[i].download_url}">
-      <div class="card-description">
-        <ul>
-          <li><b>ID:</b> ${FICHA[i].id}</li>
-          <li><b>Nome:</b> ${FICHA[i].author}</li>
-          <li><b>URL:</b> ${FICHA[i].url}</li>
-        <ul>
-      </div>
+window.addEventListener('load', () => {
+  for (let i = 0; i < FICHA.length; i++) {
+    container.innerHTML +=
+      `<div class="card">
+    <img src="${FICHA[i].download_url}">
+    <div class="card-description">
+    <ul>
+    <li><b>ID:</b> ${FICHA[i].id}</li>
+    <li><b>Nome:</b> ${FICHA[i].author}</li>
+    <li><b>URL:</b> ${FICHA[i].url}</li>
+    <ul>
+    </div>
     </div>`
   }
-  
+
+  let card = document.getElementsByClassName('card');
+  for (let i = 0; i < card.length; i++) {
+    card[i].style.backgroundColor = randomColor();
+  }
 });
